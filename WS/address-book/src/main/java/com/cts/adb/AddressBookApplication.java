7a120a4f6a01ab16c7d46b0ad9a920fe0b1e5fdd
Vamsy Kiran A <a.vamc.it@gmail.com>
2024-06-18 18:30:52 +0530

    crud app

--- WS/address-book/src/main/java/com/cts/adb/AddressBookApplication.java
+++ WS/address-book/src/main/java/com/cts/adb/AddressBookApplication.java
@@ -1,10 +1,18 @@
 package com.cts.adb;
 
+import com.cts.adb.dao.ContactDAO;
+import com.cts.adb.dao.ContactDAOInMemImpl;
+import com.cts.adb.service.ContactService;
+import com.cts.adb.service.ContactServiceImpl;
+import com.cts.adb.ui.ContactUI;
+
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ContactDAO contactDao = new ContactDAOInMemImpl();
+		ContactService contactService = new ContactServiceImpl(contactDao);
+		ContactUI contactUI = new ContactUI(contactService);
+		contactUI.run();
 	}
 
 }
